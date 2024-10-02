3794a738dec96dfe9e4b6bd3477f20b72f343314
Henrique Naylor <henriquenl09@gmail.com>
2024-10-01 23:31:03 -0300

    added cors and logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,30 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from "@nestjs/common";
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
+
     const app = await NestFactory.create(AppModule);
+    
+    // Validation
     app.useGlobalPipes(new ValidationPipe());
+
+    // Logger
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        console.log(`Received Request: [${new Date().toISOString()}] ${req.method} ${req.url} from IP: ${req.ip}`);
+        next(); 
+    });
+
+    // CORS
+    app.enableCors({
+        origin: ['https://operand-test-9abb0.web.app', 'https://operand-test-9abb0.firebaseapp.com'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    });
+
+    // Swagger
     const config = new DocumentBuilder()
     .setTitle('Tasks Api')
     .setDescription('Api to Authenticate and manage tasks on firebase')
@@ -13,6 +33,9 @@ async function bootstrap() {
     .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-    await app.listen(3000);
+
+    // Start the app
+    await app.listen(parseInt(process.env.PORT) || 3000);
 }
+
 bootstrap();
